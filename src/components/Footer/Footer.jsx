e3ff26e24061e95672088b3a3ae0d485bd6354b2
Abraham Olufeyitimi <abrahamdammy1@gmail.com>
2024-09-19 02:19:54 -0300

    add the nav icons and footer to the Homepage

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import User from "../../assets/icons/user.svg";
+import Home from "../../assets/icons/home.svg";
+import "./Footer.scss";
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <img className="footer__image" src={Home} alt="a grey icon that represents home" />
+            <div className="footer__container">
+                <img className="footer__user" src={User} alt="an icon that has the shape of a person" />
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
