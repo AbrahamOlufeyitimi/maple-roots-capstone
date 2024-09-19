435f9d73736f1716f7a88c4c9b3863e90dd2da82
Abraham Olufeyitimi <abrahamdammy1@gmail.com>
2024-09-19 14:20:47 -0300

    add footer component styling, add footer and text to homepage

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,9 @@ import "./Footer.scss";
 const Footer = () => {
     return (
         <div className="footer">
+            <div className="footer__container">
             <img className="footer__image" src={Home} alt="a grey icon that represents home" />
+            </div>
             <div className="footer__container">
                 <img className="footer__user" src={User} alt="an icon that has the shape of a person" />
             </div>
