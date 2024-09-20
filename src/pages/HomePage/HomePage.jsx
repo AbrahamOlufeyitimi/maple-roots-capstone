703f180f69bb86fbf06dc25004126589d10d106e
Abraham Olufeyitimi <abrahamdammy1@gmail.com>
2024-09-20 09:03:25 -0300

    add the checkbox and resource component, add them to the homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,8 @@
+import React from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import Screen from "../../components/Screen/Screen";
+import Resource from "../../components/Resource/Resource";
 import "./HomePage.scss";
 
 const HomePage = () => {
@@ -8,6 +10,7 @@ const HomePage = () => {
         <Screen>
             <Header />
             <p className="homepage__text">My checklist</p>
+            <Resource className="homepage__resource" text="Attend orientation session" link="https://smu.ca" />
             <Footer />
         </Screen>
     );
