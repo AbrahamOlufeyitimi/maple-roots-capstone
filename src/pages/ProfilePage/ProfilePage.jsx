0aed2af5861cf92e27c8feed78d6b8bc08b541ec
Abraham Olufeyitimi <abrahamdammy1@gmail.com>
2024-09-20 23:41:05 -0300

    add name, text and community card to profile, add styling

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,5 +1,7 @@
 import Screen from "../../components/Screen/Screen";
 import Student from "../../assets/student-pic.jpg";
+import Button from "../../components/Button/Button";
+import Footer from "../../components/Footer/Footer";
 import "./ProfilePage.scss";
 
 const ProfilePage = () => {
@@ -9,7 +11,22 @@ const ProfilePage = () => {
                 <div className="profile__container">
                     <img className="profile__image" src={Student} alt="a young male in a red jacket" />
                 </div>
+
+                <div className="profile__wrapper">
+                    <p className="profile__name">New Student</p>
+                    <p className="profile__text">Settle in with ease, thrive with confidence</p>
+                    <Button className="profile__edit">Customize Profile</Button>
+                </div>
+
+                <div className="profile__block">
+                    <p className="profile__element">Community</p>
+                    <div className="profile__card">
+                        <p className="profile__tag">Meet Students</p>
+                        <Button className="profile__btn">Connect</Button>
+                    </div>
+                </div>
             </div>
+            <Footer />
         </Screen>
     );
 };
